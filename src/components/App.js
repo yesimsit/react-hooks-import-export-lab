1f99dc66134e7543c341a71b956b9a9333585fb4
yesim <sityesim@gmail.com>
2022-06-03 08:51:04 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "./About.js"
+import NavBar from "./NavBar.js"
+import Home from "./Home.js"
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import username from "../data/user.js"
+import city from "../data/user.js"
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home
